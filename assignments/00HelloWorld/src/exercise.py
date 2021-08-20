80994a2fa3cc31c21e38a2fafb31ee0938421816
ValGar20 <a01742469@tec.mx>
2021-08-20 14:38:50 +0000

    resuelto el ejercicio par-impar

--- assignments/00HelloWorld/src/exercise.py
+++ assignments/00HelloWorld/src/exercise.py
@@ -1,6 +1,20 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    n = int(input('Dame un número:')) 
+
+    if n >= 0: 
+        if n % 2 == 0: 
+            print('Positivo par')
+        else: 
+             print('Positivo impar')
+    else: 
+        if n % 2 == 0:
+            print('Negativo par')   
+        else: 
+            print('Negativo impar')          
+    print('Adiós')
+
+
 
 if __name__=='__main__':
     main()
